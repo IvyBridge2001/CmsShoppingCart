d08fbca063c137a1cc50154e7f5ece50f0bda941
dan <dan@DESKTOP-UR7KSST>
2022-01-30 01:00:47 -0300

    Added first Admin Pages methods

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,41 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pagesList);
+        }
+
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+        //public ActionResult()
+        //{
+        //   return View();
+
+        //}
+        //public ActionResult()
+        //{
+        //  return View();
+
+        //}
+    }
+}
